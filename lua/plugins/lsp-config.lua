842719d804ff88762ecf08dbae3f7ee24bcaf0f3
holmen1 <holmen1@gmail.com>
2024-11-01 18:01:36 +0100

    Add lsp code actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,6 +25,8 @@ return {
       lspconfig.hls.setup({})
       lspconfig.lua_ls.setup({})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end
   }
 }
